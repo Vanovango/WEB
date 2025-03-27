85c705c4cbfb30aa4e0ef9b2524d5bc399f02618
Vanovango <voyten2003@bk.ru>
2025-03-27 16:57:19 +0300

    try

--- TryWeb/main/views.py
+++ TryWeb/main/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')   # отрабатывает запуск html файла
+    data = {
+        'title': 'Технопарк',
+        'values': ['Технопарк', '-', 'это', 'люди'],
+        'peoples': {
+            'staraschuk': 'Yarik',
+            'voytenko': 'Ivan',
+            'libchuk': 'Ilya'
+        }
+    }
+    return render(request, 'main/index.html', data)   # отрабатывает запуск html файла
 
 
 def about(request):
